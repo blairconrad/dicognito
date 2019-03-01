b8c177955d0599b2a617440a9a38d0ee4429749f
Blair Conrad <blair@blairconrad.com>
2019-03-01 07:01:05 -0500

    Extract data-loading methods

--- dicognito/tests/test_anonymize_single_instance.py
+++ dicognito/tests/test_anonymize_single_instance.py
@@ -1,107 +1,15 @@
-import os
 import datetime
-import pydicom
 import pytest
-from pydicom.data import get_testdata_files
 
 from dicognito.anonymizer import Anonymizer
 from dicognito.pnanonymizer import PNAnonymizer
 
+from data_for_tests import load_minimal_instance
+from data_for_tests import load_test_instance
+
 LAST = 0
 FIRST = 1
 MIDDLE = 2
-
-
-def load_minimal_instance():
-    return load_dcm(get_testdata_files('MR_small.dcm')[0])
-
-
-def load_test_instance():
-    dataset = load_minimal_instance()
-    source_image_dataset = pydicom.dataset.Dataset()
-    source_image_dataset.ReferencedSOPClassUID = ['1.2.3.0.1']
-    source_image_dataset.ReferencedSOPInstanceUID = ['1.2.3.1.1']
-    dataset.SourceImageSequence = pydicom.sequence.Sequence(
-        [source_image_dataset])
-
-    dataset.OperatorsName = "OPERATOR^FIRST^MIDDLE"
-    dataset.NameOfPhysiciansReadingStudy = "READING^FIRST^MIDDLE"
-    dataset.PerformingPhysicianName = "PERFORMING^FIRST^MIDDLE"
-    dataset.ReferringPhysicianName = "REFERRING^FIRST^MIDDLE"
-    dataset.RequestingPhysician = "REQUESTING^FIRST^MIDDLE"
-    dataset.ResponsiblePerson = "RESPONSIBLE^FIRST^MIDDLE"
-    dataset.PatientBirthName = "PBN",
-    dataset.PatientMotherBirthName = "PMBN",
-
-    dataset.PatientAddress = '10 REAL STREET'
-    dataset.RegionOfResidence = 'BROAD COVE'
-    dataset.CountryOfResidence = 'GERMANY'
-
-    dataset.IssuerOfPatientID = "ISSUEROFPATIENTID"
-    dataset.OtherPatientIDs = 'OTHERPATIENTID'
-    dataset.PerformedProcedureStepID = 'PERFORMEDID'
-    dataset.ScheduledProcedureStepID = 'SCHEDULEDID'
-
-    dataset.Occupation = "VIGILANTE"
-    dataset.PatientInsurancePlanCodeSequence = [
-        code("VALUE", "DESIGNATOR", "MEANING")]
-    dataset.MilitaryRank = "YEOMAN"
-    dataset.BranchOfService = "COAST GUARD"
-    dataset.PatientTelephoneNumbers = "123-456-7890"
-    dataset.PatientTelecomInformation = "123-456-7890"
-    dataset.PatientReligiousPreference = "PRIVATE"
-    dataset.MedicalRecordLocator = "FILING CABINET 1"
-    dataset.ReferencedPatientPhotoSequence = [referenced_photo_item()]
-    dataset.ResponsibleOrganization = "RESPONSIBLE ORGANIZATION"
-
-    other_patient_id_item0 = pydicom.dataset.Dataset()
-    other_patient_id_item0.PatientID = "opi-0-ID"
-    other_patient_id_item0.IssuerOfPatientID = "ISSUER"
-    other_patient_id_item1 = pydicom.dataset.Dataset()
-    other_patient_id_item1.PatientID = "opi-1-ID"
-    other_patient_id_item1.IssuerOfPatientID = "ISSUER"
-    dataset.OtherPatientIDsSequence = pydicom.sequence.Sequence(
-        [other_patient_id_item0, other_patient_id_item1]
-    )
-
-    request_attribute_item = pydicom.dataset.Dataset()
-    request_attribute_item.RequestedProcedureID = "rai-0-REQUESTEDID"
-    request_attribute_item.ScheduledProcedureStepID = "rai-0-SCHEDULEDID"
-    dataset.RequestAttributesSequence = pydicom.sequence.Sequence(
-        [request_attribute_item]
-    )
-
-    dataset.InstitutionName = "INSTITUTIONNAME"
-    dataset.InstitutionAddress = "INSTITUTIONADDRESS"
-    dataset.InstitutionalDepartmentName = "INSTITUTIONALDEPARTMENTNAME"
-    dataset.StationName = "STATIONNAME"
-
-    dataset.RequestingService = "REQESTINGSERVICE"
-    dataset.CurrentPatientLocation = "PATIENTLOCATION"
-
-    return dataset
-
-
-def code(value, designator, meaning):
-    code_ds = pydicom.dataset.Dataset()
-    code_ds.CodeValue = value
-    code_ds.CodingSchemeDesignator = designator
-    code_ds.CodeMeaning = meaning
-    return code_ds
-
-
-def referenced_photo_item():
-    referenced_sop_item = pydicom.dataset.Dataset()
-    referenced_sop_item.ReferencedSOPClassUID = "2.3.4.5.6.7"
-    referenced_sop_item.ReferencedSOPInstanceUID = "2.3.4.5.6.7.1.2.3"
-
-    item = pydicom.dataset.Dataset()
-    item.TypeOfInstances = "DICOM"
-    item.StudyInstanceUID = "1.2.3.4.5.6"
-    item.SeriesInstanceUID = "1.2.3.4.5.6.1"
-    item.ReferencedSOPSequence = [referenced_sop_item]
-
-    return item
 
 
 def test_minimal_instance_anonymizes_safely():
@@ -546,8 +454,3 @@ def test_datetime_of_various_lengths_gets_anonymized(acquisition_datetime):
 
     assert new_datetime_string != acquisition_datetime
     assert len(new_datetime_string) == len(acquisition_datetime)
-
-
-def load_dcm(filename):
-    script_dir = os.path.dirname(__file__)
-    return pydicom.dcmread(os.path.join(script_dir, filename))
