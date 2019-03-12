3fe38a1c67e403066ef46e9425555b6230d4f5e9
Blair Conrad <blair@blairconrad.com>
2019-03-11 22:11:24 -0400

    Set version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dicognito",
-    version="0.1.4",
+    version="0.2.0",
     author="Blair Conrad",
     author_email="blair@blairconrad.com",
     description="A tool for anonymizing DICOM files",
