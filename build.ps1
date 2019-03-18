acd31d7886d79eec5a390a06210571a5f7b3de04
Blair Conrad <blair@blairconrad.com>
2019-03-18 18:42:29 -0400

    Add ability to run subset of tests

--- build.ps1
+++ build.ps1
@@ -1,18 +1,24 @@
-if ($Args.Count -eq 0) {
-    $commands = 'clean', 'test', 'pack'
-}
-else {
-    $commands = $Args
-}
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true, Position = 0, ValueFromRemainingArguments = $true)]
+    [string[]] $Commands,
+
+    [Parameter(Mandatory = $false)]
+    [string[]] $MatchingTests
+)
 
 $env:PYTHONPATH = "${env:PYTHONPATH};$PWD"
 
 $env:PYTHONPATH = "${env:PYTHONPATH};$PWD"
+
+if ($MatchingTests) {
+    $pyTestMatching = "-k " + ($MatchingTests -join " or ")
+}
 
 $actions = @{
     'clean'        = { 'build', 'dicognito.egg-info', 'dist' | Where-Object { Test-Path $_ } | Remove-Item -Force -Recurse  }
-    'testlocal'    = { Push-Location src; python -m pytest ..\tests; Pop-Location }
-    'testforever'  = { Set-Location src; python -m pytest --looponfail ..\tests }
+    'testlocal'    = { $env:PYTHONPATH = "src;$env:PYTHONPATH"; python -m pytest $pyTestMatching tests }
+    'testforever'  = { $env:PYTHONPATH = "src;$env:PYTHONPATH"; `python -m pytest --looponfail $pyTestMatching tests }
     'smoketest'    = { python smoketest\smoketest.py; code --diff smoketest\original.txt smoketest\anonymized.txt }
     'pack'         = { python setup.py sdist bdist_wheel }
     'publish'      = { python -m twine upload dist/* }
@@ -20,7 +26,7 @@ $actions = @{
     'help'         = { Write-Output "Usage: `r`n  build command [command...]`r`n`r`nwhere commands are:`r`n  $(($actions.Keys | Sort-Object) -join "`r`n  ")`r`n" }
 }
 
-$commands | ForEach-Object {
+$Commands | ForEach-Object {
     $command = $_
     $action = $actions[$command]
     if ($action) {
