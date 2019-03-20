8103b2808d6d9849ac3c41fa3dc05218f3a1c6f7
Blair Conrad <blair@blairconrad.com>
2019-03-20 07:22:51 -0400

    Make commandline tests easier to write, harder to read

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -1,17 +1,27 @@
+import sys
 import os.path
 import shutil
 import dicognito.__main__
 from .data_for_tests import load_dcm
 
 
+def setup_module(module):
+    base_dir = os.path.dirname(__file__)
+    data_dir = os.path.join(base_dir, "data")
+    orig_dir = os.path.join(base_dir, "orig_data")
+    if os.path.isdir(data_dir):
+        shutil.rmtree(data_dir)
+    shutil.copytree(orig_dir, data_dir)
+
+
 def test_overwrite_files(capsys):
-    orig_dataset = read_file(test_overwrite_files.__name__, "orig_p01_s01_s01_i01.dcm")
+    test_name = get_test_name()
+    orig_dataset = read_original_file(test_name, "p01_s01_s01_i01.dcm")
     assert "CompressedSamples^MR1" == orig_dataset.PatientName
 
-    copy_file(test_overwrite_files.__name__, "orig_p01_s01_s01_i01.dcm", "p01_s01_s01_i01.dcm")
-    run_dicognito(test_overwrite_files.__name__, capsys)
+    run_dicognito(capsys, path_to("p*"))
 
-    anon_dataset = read_file(test_overwrite_files.__name__, "p01_s01_s01_i01.dcm")
+    anon_dataset = read_file(test_name, "p01_s01_s01_i01.dcm")
     assert anon_dataset.PatientName != orig_dataset.PatientName
 
 
@@ -19,44 +29,37 @@ def test_summary_mixed_files_reports_on_each_study(capsys):
     expected = """\
 Accession Number Patient ID       Patient Name
 ---------------- ----------       ------------
-821CFB2XY4A8     XRP26N7QWFM3     BYERS^YOLANDA^MYLES
-0ID4RQFGHICH     YKQBAJZS5UO3     MARSHALL^BEULAH^AURELIA
-NO4NP4PXEOWT     YKQBAJZS5UO3     MARSHALL^BEULAH^AURELIA
+DRVN05NEDUYD     2S183ZNON7HU     RICHMOND^MARCY^NITA
+8NZGNEJWE7QA     NPC1XHSJT51Z     MORROW^SUSANNA^LUCIEN
+SXJXM4HE90EO     NPC1XHSJT51Z     MORROW^SUSANNA^LUCIEN
 """
-
-    copy_file(
-        test_summary_mixed_files_reports_on_each_study.__name__, "orig_p01_s01_s01_i01.dcm", "p01_s01_s01_i01.dcm"
-    )
-    copy_file(
-        test_summary_mixed_files_reports_on_each_study.__name__, "orig_p01_s01_s01_i02.dcm", "p01_s01_s01_i02.dcm"
-    )
-    copy_file(
-        test_summary_mixed_files_reports_on_each_study.__name__, "orig_p02_s01_s01_i01.dcm", "p02_s01_s01_i01.dcm"
-    )
-    copy_file(
-        test_summary_mixed_files_reports_on_each_study.__name__, "orig_p02_s02_s01_i01.dcm", "p02_s02_s01_i01.dcm"
-    )
-
-    actual = run_dicognito(test_summary_mixed_files_reports_on_each_study.__name__, capsys)
+    actual = run_dicognito(capsys, path_to("p*"))
 
     assert expected == actual
 
 
 def test_summary_with_quiet_no_report(capsys):
-    copy_file(test_summary_with_quiet_no_report.__name__, "orig_p01_s01_s01_i01.dcm", "p01_s01_s01_i01.dcm")
-    copy_file(test_summary_with_quiet_no_report.__name__, "orig_p01_s01_s01_i02.dcm", "p01_s01_s01_i02.dcm")
-    copy_file(test_summary_with_quiet_no_report.__name__, "orig_p02_s01_s01_i01.dcm", "p02_s01_s01_i01.dcm")
-    copy_file(test_summary_with_quiet_no_report.__name__, "orig_p02_s01_s01_i01.dcm", "p02_s01_s01_i01.dcm")
-
-    actual = run_dicognito(test_summary_with_quiet_no_report.__name__, capsys, "--quiet")
+    actual = run_dicognito(capsys, path_to("p*"), "--quiet")
     expected = ""
     assert expected == actual
 
 
-def run_dicognito(test_name, capsys, *extra_args):
+def get_test_name():
+    depth = 1
+    while True:
+        frame = sys._getframe(depth)
+        if frame.f_code.co_name.startswith("test"):
+            return frame.f_code.co_name
+        depth += 1
+
+
+def path_to(end_of_path):
     base_dir = os.path.dirname(__file__)
-    glob = os.path.join(base_dir, "data", test_name, "p*.dcm")
-    dicognito.__main__.main(("--salt", test_name, glob) + extra_args)
+    return os.path.join(base_dir, "data", get_test_name(), end_of_path)
+
+
+def run_dicognito(capsys, *extra_args):
+    dicognito.__main__.main(("--salt", "salt for test") + extra_args)
     (out, error) = capsys.readouterr()
     return out
 
@@ -65,9 +68,5 @@ def read_file(directory, name):
     return load_dcm(os.path.join("data", directory, name))
 
 
-def copy_file(test_name, original_name, new_name):
-    base_dir = os.path.dirname(__file__)
-    original_path = os.path.join(base_dir, "data", test_name, original_name)
-    new_path = os.path.join(base_dir, "data", test_name, new_name)
-
-    shutil.copyfile(original_path, new_path)
+def read_original_file(directory, name):
+    return load_dcm(os.path.join("orig_data", directory, name))
