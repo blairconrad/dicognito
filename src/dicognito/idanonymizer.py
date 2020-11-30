608b8348961b87f27c7e2b79cf08fdadd6dbc135
Blair Conrad <blair@blairconrad.com>
2020-11-30 14:07:09 -0500

    Annotate IDAnonymizer

--- src/dicognito/idanonymizer.py
+++ src/dicognito/idanonymizer.py
@@ -1,10 +1,13 @@
+from typing import Any
 import pydicom
+
+from dicognito.randomizer import Randomizer
 
 
 class IDAnonymizer:
     _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-    def __init__(self, randomizer, id_prefix, id_suffix, *keywords):
+    def __init__(self, randomizer: Randomizer, id_prefix: str, id_suffix: str, *keywords: str):
         """\
         Create a new IDAnonymizer.
 
@@ -31,7 +34,7 @@ class IDAnonymizer:
         total_affixes_length = len(self.id_prefix) + len(self.id_suffix)
         self._indices_for_randomizer = [len(self._alphabet)] * (12 - total_affixes_length)
 
-    def __call__(self, dataset, data_element):
+    def __call__(self, dataset: pydicom.dataset.Dataset, data_element: pydicom.DataElement) -> bool:
         """\
         Potentially anonymize a single DataElement, replacing its
         value with something that obscures the patient's identity.
@@ -65,7 +68,9 @@ class IDAnonymizer:
             return True
         return False
 
-    def _anonymize_mitra_global_patient_id(self, dataset, data_element):
+    def _anonymize_mitra_global_patient_id(
+        self, dataset: pydicom.dataset.Dataset, data_element: pydicom.DataElement
+    ) -> bool:
         if data_element.tag.group == 0x0031 and data_element.tag.element % 0x0020 == 0:
             private_tag_group = data_element.tag.element >> 8
             if dataset[(0x0031 << 16) + private_tag_group].value == "MITRA LINKED ATTRIBUTES 1.0":
@@ -74,13 +79,13 @@ class IDAnonymizer:
                 return True
         return False
 
-    def _replace_id(self, data_element):
+    def _replace_id(self, data_element: pydicom.DataElement) -> None:
         if isinstance(data_element.value, pydicom.multival.MultiValue):
             data_element.value = [self._new_id(id) for id in data_element.value]
         else:
             data_element.value = self._new_id(data_element.value)
 
-    def _new_id(self, original_value):
+    def _new_id(self, original_value: Any) -> str:
         indexes = self.randomizer.get_ints_from_ranges(original_value, *self._indices_for_randomizer)
         id_root = "".join([self._alphabet[i] for i in indexes])
         return self.id_prefix + id_root + self.id_suffix
