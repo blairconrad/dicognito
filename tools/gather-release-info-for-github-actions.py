739c1d5fc2a900ee920ea4d16a183b2387d9b5d8
Blair Conrad <blair@blairconrad.com>
2024-09-29 09:26:28 -0400

    Fix multi-line output parameters in build job
    On last run, the release notes were created with %0A instead of newlines.

--- tools/gather-release-info-for-github-actions.py
+++ tools/gather-release-info-for-github-actions.py
@@ -1,6 +1,19 @@
 import os
 import re
 
+
+def set_output_parameter(name: str, value: str) -> None:
+    with open(os.environ["GITHUB_OUTPUT"], mode="a") as github_output:
+        print(f"{name}={value}", file=github_output)
+
+
+def set_multiline_output_parameter(name: str, value: str) -> None:
+    with open(os.environ["GITHUB_OUTPUT"], mode="a") as github_output:
+        print(f"{name}<<END_OF_{name}", file=github_output)
+        print(value, file=github_output)
+        print(f"END_OF_{name}", file=github_output)
+
+
 with open("src/dicognito/release_notes.md") as release_notes:
     first_line = release_notes.readline()
     if first_line.startswith("## "):
@@ -17,9 +30,6 @@ with open("src/dicognito/release_notes.md") as release_notes:
                 break
             release_body += line
 
-        release_body = release_body.strip().replace("%", "%25").replace("\n", "%0A").replace("\r", "%0D")
-
-        with open(os.environ["GITHUB_OUTPUT"], mode="a") as github_output:
-            print(f"release-name={release_name}", file=github_output)
-            print(f"is-prerelease={is_prerelease}", file=github_output)
-            print(f"release-body={release_body}", file=github_output)
+        set_output_parameter("release-name", release_name)
+        set_output_parameter("is-prerelease", is_prerelease)
+        set_multiline_output_parameter("release-body", release_body)
